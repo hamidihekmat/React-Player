f4070bedef70cfb55bb249afa721e7ebab3192fc
Hekmat Hamidi <theonehamidi@gmail.com>
2020-11-20 01:51:39 -0500

    fix forward and previous bug

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
 import data from "./util";
-import { useSongs, useCurrentSong } from "./store/store";
+import { useSongs, useCurrentSong, useIsPlaying } from "./store/store";
 
 function App() {
   // Zustand
@@ -13,25 +13,27 @@ function App() {
   const setSongs = useSongs((state) => state.setSongs);
   const currentSong = useCurrentSong((state) => state.currentSong);
   const setCurrentSong = useCurrentSong((state) => state.setCurrentSong);
-
+  const play = useIsPlaying((state) => state.play);
   // Zustand
   const [libraryState, setLibraryState] = useState(false);
-  // const [songs] = useState(data());
-  // const [currentSong, setCurrentSong] = useState({});
   const forwardHandler = () => {
     const currentIndex = songs.indexOf(currentSong);
     if (currentIndex + 1 < songs.length) {
       setCurrentSong(songs[currentIndex + 1]);
+      play();
     } else {
       setCurrentSong(songs[0]);
+      play();
     }
   };
   const previousHandler = () => {
     const currentIndex = songs.indexOf(currentSong);
     if (currentIndex === 0) {
       setCurrentSong(songs[songs.length - 1]);
+      play();
     } else {
       setCurrentSong(songs[currentIndex - 1]);
+      play();
     }
   };
 
